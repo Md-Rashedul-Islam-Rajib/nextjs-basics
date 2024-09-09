c418061055d90edac3f6ff88076d100c58924340
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-10 02:57:13 +0600

    using ISR concept with fetch function property revalidate

--- src/lib/getComment.js
+++ src/lib/getComment.js
@@ -1,5 +1,7 @@
 const getComments = async () => {
-    const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const comments = await fetch("https://jsonplaceholder.typicode.com/comments",{
+        next: { revalidate : 7200}
+    });
     const data = await comments.json();
     return data;
   };
@@ -7,7 +9,7 @@ const getComments = async () => {
   export default getComments;
   
   export async function getPosts() {
-    const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts", { next : { revalidate : 3600}});
     const data = await posts.json();
     return data;
   }
