1a227dadc82e69fd1340e10782e87e9e4351a061
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-19 02:16:18 +0600

    accordion added

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,26 +1,55 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-const Details =async ({params}: {params : {id:string}}) => {
-    let photo;
-    try {
-        const data = await fetch(`https://jsonplaceholder.typicode.com/photos/${params.id}`, {cache: 'no-store'});
-        const res = await data.json();
-        photo= res;
-    } catch (error:any) {
-        throw new Error(error)
-    }
-    console.log(photo)
+const Details = async ({ params }: { params: { id: string } }) => {
+  let photo;
+  try {
+    const data = await fetch(
+      `https://jsonplaceholder.typicode.com/photos/${params.id}`,
+      { cache: "no-store" }
+    );
+    const res = await data.json();
+    photo = res;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+  console.log(photo);
+
+  const faq = [
+    {
+      ques: "Question 1?",
+      ans: "answer 1",
+    },
+    {
+      ques: "Question 2?",
+      ans: "answer 2",
+    },
+    {
+      ques: "Question 3?",
+      ans: "answer 3",
+    },
+  ];
   return (
-    <div>
-      <Image 
-      alt=''
-      src={photo.url}
-      width={400}
-      height={400}
-      ></Image>
-    </div>
-  )
-}
+    <div className="grid grid-cols-1 gap-6">
+      <Image alt="" src={photo.url} width={400} height={400}></Image>
 
-export default Details
+      <div className="w-1/4 flex justify-center">
+        <Accordion type="multiple" collapsible>
+          {faq.map((item, idx) => (
+            <AccordionItem key={idx} value={`item-${idx}`}>
+              <AccordionTrigger>{item.ques}</AccordionTrigger>
+              <AccordionContent>{item.ans}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
