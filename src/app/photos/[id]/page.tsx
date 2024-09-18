364e1e33aa1e0fd233c4e9c1d9187fffa2edbbab
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-19 02:19:27 +0600

    alert dialog added

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -5,6 +5,17 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const Details = async ({ params }: { params: { id: string } }) => {
   let photo;
@@ -48,6 +59,22 @@ const Details = async ({ params }: { params: { id: string } }) => {
           ))}
         </Accordion>
       </div>
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
